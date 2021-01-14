7e86e48a0f850991ba5cf221d52d928688fddcff
tech-gian <sdi1900059@di.uoa.gr>
2021-01-14 10:23:29 +0200

    Add, functions to module

--- Database/db_module.py
+++ Database/db_module.py
@@ -7,6 +7,11 @@ import sqlite3
 
 
 
+# Helping functions
+def list_sort(item):
+    return item[0]
+
+
 # Class Database
 class Database:
 
@@ -54,15 +59,59 @@ class Database:
         conn.commit()
         conn.close()
 
+        return True
+
 
     # Delete old registration, by name
     def delete(self, name):
-        print("TODO")
+        # Check if name exists in table
+        if self.name_to_num.has_key(name):
+            # Removing elements from dicts
+            number = self.name_to_num.get(name)
+            self.name_to_num.pop(name)
+            self.num_to_name.pop(number)
+
+            # Removing register from db
+            # TODO: check if table exists
+            conn = sqlite3.connect(self.name + ".db")
+            cur = conn.cursor()
+
+            cur.execute("delete from " + self.name + " where id=?", name)
+            
+            # Closing connection
+            conn.commit()
+            conn.close()
+            return True
+        else:
+            print("No contact with name: " + name + ", found")
+            return False
+
+
 
     
     # Delete old registration, by number
     def delete(self, number):
-        print("TODO")
+        # Check if number exists in table
+        if self.num_to_name.has_key(number):
+            # Removing elements from dicts
+            name = self.num_to_name.get(number)
+            self.num_to_name.pop(number)
+            self.name_to_num.pop(name)
+
+            # Removing register from db
+            # TODO: check if table exists
+            conn = sqlite3.connect(self.name + ".db")
+            cur = conn.cursor()
+
+            cur.execute("delete from " + self.name + " where id=?", name)
+
+            # Closing connection
+            conn.commit()
+            conn.close()
+            return True
+        else:
+            print("No contact with number: " + number + ", found")
+            return False
 
 
     # Find a contact, by name
@@ -70,21 +119,52 @@ class Database:
         # Check if name exists in table
         if self.name_to_num.has_key(name):
             print("Name: " + name + ", Phone_number: " + self.name_to_num.get(name))
+            return True
         else:
             print("Contact not found")
+            return False
 
 
     # Find a contact, by number
     def find(self, number):
-        # Check if name exists in table
+        # Check if number exists in table
         if self.num_to_name.has_key(number):
             print("Name: " + self.num_to_name.get(number) + ", Phone_number: " + number)
+            return True
         else:
             print("Contact not found")
+            return False
 
 
     # Print all contacts
     def print(self):
-        print("TODO")
+        while (True):
+            ans = input("Show your contacts with the order you entered them or alphbetical? (Type 'o' or 'a'): ")
+            if type(ans) != str:
+                print("This isn't a valid answer. Type again")
+            else:
+                ans = str(ans)
 
+                # For entered order
+                if ans == 'o':
+                    for item in self.name_to_num:
+                        print(item + " " + self.name_to_num.get(item))
+
+                    break
+                # For alphabetical order
+                elif ans == 'a':
+                    temp = []
+                    for item in self.name_to_num:
+                        tup = tuple(item, self.name_to_num.get(item))
+                        temp.append(tup)
+                    
+                    # TODO: check that list_sort() works fine
+                    temp.sort(key=list_sort)
+                    for item in temp:
+                        print(item[0] + " " + item[1])
+                    
+                    break
+                else:
+                    print("This isn't a valid answer. Type again")
+                
 
