d58ae7e4e450f457c23481a21c1904f59b3f6154
tech-gian <sdi1900059@di.uoa.gr>
2021-01-15 17:37:33 +0200

    Add, test_create()

--- Tests/db_module_test.py
+++ Tests/db_module_test.py
@@ -6,9 +6,25 @@
 
 import unittest
 import sys
+import string
+import random
+import os
+from os import path
 
 sys.path.append("../Database")
 from db_module import Database
+
+
+
+# Helping Functions
+
+
+def get_random_string(len):
+    # Generates random string with upper and lower case letters
+    letters = string.ascii_letters
+    temp = ''.join(random.choice(letters) for i in range(len))
+    return temp
+
 
 
 # Class for Database_Test
@@ -17,7 +33,18 @@ class Database_Test(unittest.TestCase):
 
     # Testing if db creation works fine
     def test_create(self):
-        pass
+        DB_NAME = get_random_string(10)
+
+        # Create db
+        db = Database(DB_NAME)
+
+        # Check if file exists
+        self.assertTrue(os.path.exists(DB_NAME + ".db"))
+        self.assertEqual(db.size(), 0)
+
+        # Delete file
+        os.remove(DB_NAME + ".db")
+
 
     # Testing if inserting contact works fine
     def test_add(self):
