a778e4b8429a4b54f75bf8b196181f0b8d650c0b
tech-gian <sdi1900059@di.uoa.gr>
2021-01-16 01:51:10 +0200

    Modify, test_delete()

--- Tests/db_module_test.py
+++ Tests/db_module_test.py
@@ -48,11 +48,66 @@ class Database_Test(unittest.TestCase):
 
     # Testing if inserting contact works fine
     def test_add(self):
-        pass
+        DB_NAME = get_random_string(10)
+
+        # Create db
+        db = Database(DB_NAME)
+
+
+        # Inserting names with phones
+        for i in range(0, 100):
+            if i < 10:
+                num = "692030400"
+            else:
+                num = "69203040"
+            
+            # Check if returned True
+            self.assertTrue(db.add("test_name" + str(i), num + str(i)))
+
+            # Check if inserted
+            self.assertEqual(db.size(), i+1)
+
+
+        # Check if we try to insert an existing contact
+        self.assertFalse(db.add("test_name1", "6920304001"))
+        self.assertEqual(db.size(), 100)
+
+
+        # Delete file
+        os.remove(DB_NAME + ".db")
+
+
 
     # Testing if deleting contact works fine
     def test_delete(self):
-        pass
+        DB_NAME = get_random_string(10)
+
+
+        # Create db
+        db = Database(DB_NAME)
+
+        # Insert names with phones
+        for i in range(0, 100):
+            if i < 10:
+                num = "692030400"
+            else:
+                num = "69203040"
+
+            db.add("test_name" + str(i), num + str(i))
+
+        # Delete a contact by name
+        self.assertTrue(db.delete_name("test_name63"))
+        self.assertFalse(db.delete_name("no_existing"))
+        self.assertEqual(db.size(), 99)
+
+        # Delete a contact by number
+        self.assertTrue(db.delete_num("6920304082"))
+        self.assertFalse(db.delete_num("8150607080"))
+        self.assertEqual(db.size(), 98)
+
+        os.remove(DB_NAME + ".db")
+
+
 
     # Testing if finding contact works fine
     def test_find(self):
